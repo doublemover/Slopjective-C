e1576ff03c9cf50d3596d14bd5db20a4630d9d7c
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-09 04:48:13 -0400

    HC split AST type surface owners

--- native/objc3c/src/ast/objc3_ast_type_surface.h
+++ native/objc3c/src/ast/objc3_ast_type_surface.h
@@ -1,16 +1,4 @@
 #pragma once
 
-#include "ast/objc3_ast_core.h"
-
-#include <string>
-#include <vector>
-
-const char *Objc3ValueTypeSpelling(ValueType type);
-bool Objc3ValueTypeIsScalar(ValueType type);
-bool Objc3ValueTypeIsObjectReference(ValueType type);
-bool Objc3ValueTypeIsRuntimeMetadataReference(ValueType type);
-bool Objc3FuncParamHasConcreteTypeSurface(const FuncParam &param);
-std::string Objc3FuncParamTypeReplayKey(const FuncParam &param);
-std::string Objc3CallableSignatureReplayKey(
-    const std::string &name, const std::vector<FuncParam> &params,
-    ValueType return_type, bool async_declared, bool throws_declared);
+#include "ast/objc3_ast_callable_type_surface.h"
+#include "ast/objc3_ast_value_type_queries.h"
---
+++ native/objc3c/src/ast/objc3_ast_value_type_queries.cpp
@@ -0,0 +1,46 @@
+#include "ast/objc3_ast_value_type_queries.h"
+
+const char *Objc3ValueTypeSpelling(ValueType type) {
+  switch (type) {
+    case ValueType::I32:
+      return "i32";
+    case ValueType::Bool:
+      return "bool";
+    case ValueType::Void:
+      return "void";
+    case ValueType::Function:
+      return "function";
+    case ValueType::ObjCId:
+      return "id";
+    case ValueType::ObjCClass:
+      return "Class";
+    case ValueType::ObjCSel:
+      return "SEL";
+    case ValueType::ObjCProtocol:
+      return "Protocol";
+    case ValueType::ObjCInstancetype:
+      return "instancetype";
+    case ValueType::ObjCObjectPtr:
+      return "object-ptr";
+    case ValueType::Unknown:
+    default:
+      return "unknown";
+  }
+}
+
+bool Objc3ValueTypeIsScalar(ValueType type) {
+  return type == ValueType::I32 || type == ValueType::Bool ||
+         type == ValueType::Void;
+}
+
+bool Objc3ValueTypeIsObjectReference(ValueType type) {
+  return type == ValueType::ObjCId || type == ValueType::ObjCClass ||
+         type == ValueType::ObjCInstancetype ||
+         type == ValueType::ObjCObjectPtr;
+}
+
+bool Objc3ValueTypeIsRuntimeMetadataReference(ValueType type) {
+  return type == ValueType::ObjCClass || type == ValueType::ObjCSel ||
+         type == ValueType::ObjCProtocol ||
+         type == ValueType::ObjCInstancetype;
+}
