be384d50aac1d01ed4c05830e9a548625ac9bb27
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-09 19:08:42 -0400

    refactor(json): extract schema ref contract

--- native/objc3c/src/io/json/json_schema.cpp
+++ native/objc3c/src/io/json/json_schema.cpp
@@ -1,6 +1,5 @@
 #include "io/json/json_schema.h"
 
-#include "io/json/json_pointer.h"
 #include "io/json/json_schema_annotation_contract_validation.h"
 #include "io/json/json_schema_applicator_contract_validation.h"
 #include "io/json/json_schema_composition_contract_validation.h"
@@ -9,6 +8,7 @@
 #include "io/json/json_schema_errors.h"
 #include "io/json/json_schema_numeric_contract_validation.h"
 #include "io/json/json_schema_pattern_contract_validation.h"
+#include "io/json/json_schema_ref_contract_validation.h"
 #include "io/json/json_schema_required_contract_validation.h"
 #include "io/json/json_schema_type_contract_validation.h"
 #include "io/json/json_schema_unique_items_contract_validation.h"
@@ -28,19 +28,7 @@ void ValidateJsonSchemaNodeContract(const JsonValue &schema_root,
   }
   ValidateJsonSchemaUnsupportedKeywordContracts(schema, schema_path, result);
   ValidateJsonSchemaAnnotationContracts(schema, schema_path, result);
-
-  if (const JsonValue *ref = schema.Find("$ref"); ref != nullptr) {
-    if (!ref->IsString()) {
-      AddJsonSchemaContractError(result, "invalid_ref",
-                                 JsonSchemaKeywordPath(schema_path, "$ref"),
-                                 "$ref must be a local JSON pointer string");
-    } else if (ResolveLocalJsonPointerRef(schema_root, ref->AsString()) ==
-               nullptr) {
-      AddJsonSchemaContractError(
-          result, "unresolved_ref", JsonSchemaKeywordPath(schema_path, "$ref"),
-          "unresolved schema reference " + ref->AsString());
-    }
-  }
+  ValidateJsonSchemaRefContract(schema_root, schema, schema_path, result);
   if (const JsonValue *type = schema.Find("type"); type != nullptr) {
     ValidateJsonSchemaTypeContract(*type,
                                    JsonSchemaKeywordPath(schema_path, "type"),
