e49583f8c23192e19893db70f6b3a9047751d933
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-09 16:09:19 -0400

    HC move error lowering contract builders

--- native/objc3c/src/artifacts/objc3_frontend_error_semantic_artifacts.h
+++ native/objc3c/src/artifacts/objc3_frontend_error_semantic_artifacts.h
@@ -2,9 +2,26 @@
 
 #include <string>
 
+#include "lower/contracts/error_handling_result_bridging_contracts.h"
+#include "lower/contracts/error_handling_throws_unwind_contracts.h"
 #include "pipeline/objc3_frontend_types.h"
 
 namespace objc3::artifacts::frontend {
+
+[[nodiscard]] Objc3ThrowsPropagationLoweringContract
+BuildThrowsPropagationLoweringContract(
+    const Objc3SemaParityContractSurface &sema_parity_surface);
+
+[[nodiscard]] Objc3ResultLikeLoweringContract BuildResultLikeLoweringContract(
+    const Objc3SemaParityContractSurface &sema_parity_surface);
+
+[[nodiscard]] Objc3NSErrorBridgingLoweringContract
+BuildNSErrorBridgingLoweringContract(
+    const Objc3SemaParityContractSurface &sema_parity_surface);
+
+[[nodiscard]] Objc3UnwindCleanupLoweringContract
+BuildUnwindCleanupLoweringContract(
+    const Objc3SemaParityContractSurface &sema_parity_surface);
 
 [[nodiscard]] std::string BuildErrorHandlingErrorSemanticModelSummaryJson(
     const Objc3ErrorHandlingErrorSemanticModelSummary &summary);
