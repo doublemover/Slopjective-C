640d19bab2ed591f36686b5940943b18061ecb2c
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-09 11:02:42 -0400

    HC split IR type symbol dispatch counters

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_publication.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_publication.h
@@ -44,3 +44,6 @@ void EmitObjc3IRErrorHandlingMetadataNodes(
     const Objc3IRFrontendMetadata &metadata, std::ostringstream &out);
 
 void EmitObjc3IRConcurrencyRuntimeMetadataNodes(std::ostringstream &out);
+
+void EmitObjc3IRTypeSymbolDispatchCounterNodes(
+    const Objc3IRFrontendMetadata &metadata, std::ostringstream &out);
