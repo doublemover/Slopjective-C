328bd8fe9e4cf0d757a6212f8c4082cec256d850
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-09 05:34:34 -0400

    HC split JSON value container writers

--- native/objc3c/src/io/json/json_value_writer.cpp
+++ native/objc3c/src/io/json/json_value_writer.cpp
@@ -1,8 +1,8 @@
 #include "io/json/json_value_writer.h"
 
-#include <cstddef>
 #include <iomanip>
 
+#include "io/json/json_value_container_writer.h"
 #include "io/objc3_json.h"
 
 namespace objc3::io::json {
@@ -21,33 +21,12 @@ void WriteJsonValue(std::ostream &out, const JsonValue &value) {
     case JsonValue::Kind::kString:
       objc3::io::WriteJsonString(out, value.AsString());
       return;
-    case JsonValue::Kind::kArray: {
-      out << '[';
-      const auto &array = value.AsArray();
-      for (std::size_t i = 0; i < array.size(); ++i) {
-        if (i > 0) {
-          out << ',';
-        }
-        WriteJsonValue(out, array[i]);
-      }
-      out << ']';
+    case JsonValue::Kind::kArray:
+      WriteJsonArrayValue(out, value.AsArray());
       return;
-    }
-    case JsonValue::Kind::kObject: {
-      out << '{';
-      bool first = true;
-      for (const auto &[key, item] : value.AsObject()) {
-        if (!first) {
-          out << ',';
-        }
-        first = false;
-        objc3::io::WriteJsonString(out, key);
-        out << ':';
-        WriteJsonValue(out, item);
-      }
-      out << '}';
+    case JsonValue::Kind::kObject:
+      WriteJsonObjectValue(out, value.AsObject());
       return;
-    }
   }
 }
 
