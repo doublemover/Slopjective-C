2728c61d579c1772bbceae57c1309ec8f3173689
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-09 04:21:25 -0400

    HC remove drained runtime include shards

--- native/objc3c/src/runtime/objc3_runtime.cpp
+++ native/objc3c/src/runtime/objc3_runtime.cpp
@@ -33,9 +33,3 @@
 #include "runtime/storage/property_accessors.h"
 #include "runtime/storage/weak_slots.h"
 #include "support/selectors/selector_normalization.h"
-
-#include "runtime/objc3_runtime_parts/objc3_runtime_part_001.inc"
-#include "runtime/objc3_runtime_parts/objc3_runtime_part_002.inc"
-#include "runtime/objc3_runtime_parts/objc3_runtime_part_003.inc"
-#include "runtime/objc3_runtime_parts/objc3_runtime_part_004.inc"
-#include "runtime/objc3_runtime_parts/objc3_runtime_part_005.inc"
