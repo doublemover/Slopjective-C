415fb4e47e5ba474cf0b8f570f9be18298bd2bcf
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-09 16:12:49 -0400

    HC move cross module lowering contract builders

--- native/objc3c/src/artifacts/objc3_frontend_module_semantic_artifacts.h
+++ native/objc3c/src/artifacts/objc3_frontend_module_semantic_artifacts.h
@@ -2,9 +2,30 @@
 
 #include <string>
 
+#include "lower/contracts/cross_module_lowering_contracts.h"
 #include "pipeline/objc3_frontend_types.h"
 
 namespace objc3::artifacts::frontend {
+
+[[nodiscard]] Objc3ModuleImportGraphLoweringContract
+BuildModuleImportGraphLoweringContract(
+    const Objc3SemaParityContractSurface &sema_parity_surface);
+
+[[nodiscard]] Objc3NamespaceCollisionShadowingLoweringContract
+BuildNamespaceCollisionShadowingLoweringContract(
+    const Objc3SemaParityContractSurface &sema_parity_surface);
+
+[[nodiscard]] Objc3PublicPrivateApiPartitionLoweringContract
+BuildPublicPrivateApiPartitionLoweringContract(
+    const Objc3SemaParityContractSurface &sema_parity_surface);
+
+[[nodiscard]] Objc3IncrementalModuleCacheInvalidationLoweringContract
+BuildIncrementalModuleCacheInvalidationLoweringContract(
+    const Objc3SemaParityContractSurface &sema_parity_surface);
+
+[[nodiscard]] Objc3CrossModuleConformanceLoweringContract
+BuildCrossModuleConformanceLoweringContract(
+    const Objc3SemaParityContractSurface &sema_parity_surface);
 
 [[nodiscard]] std::string
 BuildCrossModuleSemanticContractsDiagnosticsSummaryJson(
