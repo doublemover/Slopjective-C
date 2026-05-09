2506e3519285ada132c7c14ffe630a4794ae2e6e
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-09 15:59:04 -0400

    refactor(ir): extract entry point emission

--- native/objc3c/src/ir/objc3_ir_emitter.cpp
+++ native/objc3c/src/ir/objc3_ir_emitter.cpp
@@ -20,6 +20,7 @@
 #include "ir/objc3_ir_emission_helpers.h"
 #include "ir/objc3_ir_emission_prologue.h"
 #include "ir/objc3_ir_emission_readiness_publication.h"
+#include "ir/objc3_ir_entry_point_emission.h"
 #include "ir/objc3_ir_emitter_context.h"
 #include "ir/objc3_ir_frontend_metadata_publication.h"
 #include "ir/objc3_ir_lowering_extension_metadata_publication.h"
@@ -8318,61 +8319,7 @@ class Objc3IREmitter {
   }
 
   void EmitEntryPoint(std::ostringstream &out) const {
-    auto main_it = function_arity_.find("main");
-    const bool has_zero_arity_main =
-        main_it != function_arity_.end() && main_it->second == 0;
-    if (has_zero_arity_main) {
-      out << "define i32 @objc3c_entry() {\n";
-      out << "entry:\n";
-      ValueType main_return_type = ValueType::I32;
-      const LoweredFunctionSignature *main_signature = LookupFunctionSignature("main");
-      if (main_signature != nullptr) {
-        main_return_type = main_signature->return_type;
-      }
-      const bool main_throws =
-          main_signature != nullptr && main_signature->throws_declared;
-      if (main_throws) {
-        out << "  %main.error.addr = alloca i32, align 4\n";
-        out << "  store i32 0, ptr %main.error.addr, align 4\n";
-      }
-      if (main_return_type == ValueType::Void) {
-        out << "  call void @main(";
-        if (main_throws) {
-          out << "ptr %main.error.addr";
-        }
-        out << ")\n";
-        out << "  ret i32 0\n";
-      } else {
-        out << "  %call_main = call " << LLVMScalarType(main_return_type)
-            << " @main(";
-        if (main_throws) {
-          out << "ptr %main.error.addr";
-        }
-        out << ")\n";
-        if (main_return_type == ValueType::Bool) {
-          out << "  %call_main_i32 = zext i1 %call_main to i32\n";
-          out << "  ret i32 %call_main_i32\n";
-        } else {
-          out << "  ret i32 %call_main\n";
-        }
-      }
-      out << "}\n";
-      return;
-    }
-
-    out << "define internal i32 @objc3c_entry() {\n";
-    out << "entry:\n";
-    std::string previous = "0";
-    for (std::size_t i = 0; i < program_.globals.size(); ++i) {
-      const auto &global = program_.globals[i];
-      const std::string load_name = "%entry_load_" + std::to_string(i);
-      const std::string sum_name = "%entry_sum_" + std::to_string(i);
-      out << "  " << load_name << " = load i32, ptr @" << global.name << ", align 4\n";
-      out << "  " << sum_name << " = add i32 " << previous << ", " << load_name << "\n";
-      previous = sum_name;
-    }
-    out << "  ret i32 " << previous << "\n";
-    out << "}\n";
+    EmitObjc3IREntryPoint(program_, function_arity_, function_signatures_, out);
   }
 
   const Objc3Program &program_;
