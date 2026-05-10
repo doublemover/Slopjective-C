f67011278e07acb92b28945209c3a374c4d3ac4d
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 04:25:13 -0400

    HC split IR metaprogramming local replay metadata

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_module_replay.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_module_replay.h
@@ -1,21 +1,7 @@
 #pragma once
 
-#include <cstddef>
-#include <string>
-
 #include "ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_imports.h"
-
-struct Objc3IRFrontendMetaprogrammingModuleReplayLocalMetadata {
-  std::string lowering_metaprogramming_module_interface_replay_preservation_key;
-  std::size_t metaprogramming_module_replay_local_derive_method_count = 0;
-  std::size_t metaprogramming_module_replay_local_macro_artifact_count = 0;
-  std::size_t
-      metaprogramming_module_replay_local_interface_property_behavior_artifact_count = 0;
-  std::size_t
-      metaprogramming_module_replay_local_implementation_property_behavior_artifact_count =
-          0;
-  std::size_t metaprogramming_module_replay_local_runtime_method_list_count = 0;
-};
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_local.h"
 
 struct Objc3IRFrontendMetaprogrammingModuleReplayMetadata
     : Objc3IRFrontendMetaprogrammingModuleReplayLocalMetadata,
---
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_local.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+struct Objc3IRFrontendMetaprogrammingModuleReplayLocalMetadata {
+  std::string lowering_metaprogramming_module_interface_replay_preservation_key;
+  std::size_t metaprogramming_module_replay_local_derive_method_count = 0;
+  std::size_t metaprogramming_module_replay_local_macro_artifact_count = 0;
+  std::size_t
+      metaprogramming_module_replay_local_interface_property_behavior_artifact_count = 0;
+  std::size_t
+      metaprogramming_module_replay_local_implementation_property_behavior_artifact_count =
+          0;
+  std::size_t metaprogramming_module_replay_local_runtime_method_list_count = 0;
+};
