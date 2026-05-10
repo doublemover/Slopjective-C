700d2c65737d4357bc77c82c61d985b8a0b3599c
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 00:05:08 -0400

    HC split ownership runtime API metadata rows

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_publication_runtime_semantics_dispatch_ownership_runtime_api.cpp
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_publication_runtime_semantics_dispatch_ownership_runtime_api.cpp
@@ -1,106 +1,15 @@
 #include "ir/objc3_ir_frontend_metadata_publication_runtime_semantics_dispatch_ownership_runtime_api.h"
 
-#include <sstream>
-
-#include "ir/objc3_ir_c_string.h"
-#include "ir/objc3_ir_frontend_metadata.h"
-#include "lower/objc3_lowering_contract.h"
+#include "ir/objc3_ir_frontend_metadata_publication_runtime_semantics_dispatch_ownership_memory_management.h"
+#include "ir/objc3_ir_frontend_metadata_publication_runtime_semantics_dispatch_ownership_runtime_gate.h"
+#include "ir/objc3_ir_frontend_metadata_publication_runtime_semantics_dispatch_ownership_runtime_hooks.h"
 
 void EmitObjc3IROwnershipRuntimeApiMetadataNodes(
     const Objc3IRFrontendMetadata &metadata,
     std::size_t synthesized_property_accessor_count, std::ostringstream &out) {
-  out << "!69 = !{!\""
-      << EscapeCStringLiteral(kObjc3OwnershipRuntimeHookEmissionContractId)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3OwnershipRuntimeHookEmissionAccessorModel)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3OwnershipRuntimeHookEmissionPropertyContextModel)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3OwnershipRuntimeHookEmissionAutoreleaseModel)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3OwnershipRuntimeHookEmissionFailClosedModel)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3RuntimeRetainI32Symbol)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3RuntimeReleaseI32Symbol)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3RuntimeAutoreleaseI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeReadCurrentPropertyI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeWriteCurrentPropertyI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeExchangeCurrentPropertyI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeLoadWeakCurrentPropertyI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeStoreWeakCurrentPropertyI32Symbol)
-      << "\", i64 "
-      << static_cast<unsigned long long>(synthesized_property_accessor_count)
-      << "}\n";
-  out << "!70 = !{!\""
-      << EscapeCStringLiteral(kObjc3RuntimeMemoryManagementApiContractId)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeMemoryManagementApiReferenceModel)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3RuntimeMemoryManagementApiWeakModel)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeMemoryManagementApiAutoreleasepoolModel)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeMemoryManagementApiFailClosedModel)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3RuntimeRetainI32Symbol)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3RuntimeReleaseI32Symbol)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3RuntimeAutoreleaseI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeReadCurrentPropertyI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeWriteCurrentPropertyI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeExchangeCurrentPropertyI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeLoadWeakCurrentPropertyI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeStoreWeakCurrentPropertyI32Symbol)
-      << "\", i64 "
-      << static_cast<unsigned long long>(synthesized_property_accessor_count)
-      << "}\n";
-  out << "!71 = !{!\""
-      << EscapeCStringLiteral(kObjc3RuntimeMemoryManagementImplementationContractId)
-      << "\", !\""
-      << EscapeCStringLiteral(
-             kObjc3RuntimeMemoryManagementImplementationRefcountModel)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeMemoryManagementImplementationWeakModel)
-      << "\", !\""
-      << EscapeCStringLiteral(
-             kObjc3RuntimeMemoryManagementImplementationAutoreleasepoolModel)
-      << "\", !\""
-      << EscapeCStringLiteral(
-             kObjc3RuntimeMemoryManagementImplementationFailClosedModel)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3RuntimeRetainI32Symbol)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3RuntimeReleaseI32Symbol)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3RuntimeAutoreleaseI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimePushAutoreleasepoolScopeSymbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimePopAutoreleasepoolScopeSymbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeLoadWeakCurrentPropertyI32Symbol)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3RuntimeStoreWeakCurrentPropertyI32Symbol)
-      << "\", i64 "
-      << static_cast<unsigned long long>(synthesized_property_accessor_count)
-      << ", i64 "
-      << static_cast<unsigned long long>(
-             metadata.autoreleasepool_scope_lowering_scope_sites)
-      << "}\n";
-  out << "!72 = !{!\""
-      << EscapeCStringLiteral(kObjc3OwnershipRuntimeGateContractId)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3OwnershipRuntimeGateSupportedModel)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3OwnershipRuntimeGateEvidenceModel)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3OwnershipRuntimeGateNonGoalModel)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3OwnershipRuntimeGateFailClosedModel)
-      << "\", !\""
-      << EscapeCStringLiteral(kObjc3OwnershipRuntimeHookEmissionContractId)
-      << "\", !\"" << EscapeCStringLiteral(kObjc3RuntimeMemoryManagementApiContractId)
-      << "\", !\""
-      << EscapeCStringLiteral(
-             kObjc3RuntimeMemoryManagementImplementationContractId)
-      << "\"}\n";
+  EmitObjc3IROwnershipRuntimeHookEmissionMetadataNode(
+      synthesized_property_accessor_count, out);
+  EmitObjc3IRMemoryManagementRuntimeMetadataNodes(
+      metadata, synthesized_property_accessor_count, out);
+  EmitObjc3IROwnershipRuntimeGateMetadataNode(out);
 }
