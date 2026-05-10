85c4eb57ee7be1e1c361f7174a1c2be3bab71ea4
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 04:35:14 -0400

    HC split IR metaprogramming replay readiness metadata

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_imports.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_imports.h
@@ -2,7 +2,9 @@
 
 #include <cstddef>
 
-struct Objc3IRFrontendMetaprogrammingModuleReplayImportsMetadata {
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_readiness.h"
+
+struct Objc3IRFrontendMetaprogrammingModuleReplayImportCountsMetadata {
   std::size_t metaprogramming_module_replay_imported_module_count = 0;
   std::size_t metaprogramming_module_replay_imported_derive_method_count = 0;
   std::size_t metaprogramming_module_replay_imported_macro_artifact_count = 0;
@@ -13,7 +15,8 @@ struct Objc3IRFrontendMetaprogrammingModuleReplayImportsMetadata {
       metaprogramming_module_replay_imported_implementation_property_behavior_artifact_count =
           0;
   std::size_t metaprogramming_module_replay_imported_runtime_method_list_count = 0;
-  bool metaprogramming_module_replay_runtime_import_artifact_ready = false;
-  bool metaprogramming_module_replay_separate_compilation_preservation_ready = false;
-  bool deterministic_metaprogramming_module_interface_replay_handoff = false;
 };
+
+struct Objc3IRFrontendMetaprogrammingModuleReplayImportsMetadata
+    : Objc3IRFrontendMetaprogrammingModuleReplayImportCountsMetadata,
+      Objc3IRFrontendMetaprogrammingModuleReplayReadinessMetadata {};
---
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_readiness.h
@@ -0,0 +1,7 @@
+#pragma once
+
+struct Objc3IRFrontendMetaprogrammingModuleReplayReadinessMetadata {
+  bool metaprogramming_module_replay_runtime_import_artifact_ready = false;
+  bool metaprogramming_module_replay_separate_compilation_preservation_ready = false;
+  bool deterministic_metaprogramming_module_interface_replay_handoff = false;
+};
