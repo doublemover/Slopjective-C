2ccc1e19c7917edbc1eec1f15790406d60009577
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 00:03:09 -0400

    refactor(json): split scalar value factories

--- native/objc3c/src/io/json/json_value.cpp
+++ native/objc3c/src/io/json/json_value.cpp
@@ -1,7 +1,5 @@
 #include "io/json/json_value.h"
 
-#include <utility>
-
 namespace objc3::io::json {
 namespace {
 
@@ -11,33 +9,6 @@ const std::string &EmptyString() {
 }
 
 }  // namespace
-
-JsonValue::JsonValue() = default;
-
-JsonValue JsonValue::Null() {
-  return JsonValue();
-}
-
-JsonValue JsonValue::Bool(bool value) {
-  JsonValue out;
-  out.kind_ = Kind::kBool;
-  out.bool_value_ = value;
-  return out;
-}
-
-JsonValue JsonValue::Number(double value) {
-  JsonValue out;
-  out.kind_ = Kind::kNumber;
-  out.number_value_ = value;
-  return out;
-}
-
-JsonValue JsonValue::String(std::string value) {
-  JsonValue out;
-  out.kind_ = Kind::kString;
-  out.string_value_ = std::move(value);
-  return out;
-}
 
 JsonValue::Kind JsonValue::kind() const {
   return kind_;
