a9019e5b20044608a4c2c5c62a8979cd8c243af3
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 03:36:53 -0400

    HC split IR module import graph metadata

---
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_module_import_graph.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+struct Objc3IRFrontendModuleImportGraphMetadata {
+  std::string lowering_module_import_graph_replay_key;
+  std::size_t module_import_graph_lowering_sites = 0;
+  std::size_t module_import_graph_lowering_import_edge_candidate_sites = 0;
+  std::size_t module_import_graph_lowering_namespace_segment_sites = 0;
+  std::size_t module_import_graph_lowering_object_pointer_type_sites = 0;
+  std::size_t module_import_graph_lowering_pointer_declarator_sites = 0;
+  std::size_t module_import_graph_lowering_normalized_sites = 0;
+  std::size_t module_import_graph_lowering_contract_violation_sites = 0;
+  bool deterministic_module_import_graph_lowering_handoff = false;
+  std::string lowering_namespace_collision_shadowing_replay_key;
+  std::size_t namespace_collision_shadowing_lowering_sites = 0;
+  std::size_t namespace_collision_shadowing_lowering_namespace_segment_sites = 0;
+  std::size_t
+      namespace_collision_shadowing_lowering_import_edge_candidate_sites = 0;
+  std::size_t
+      namespace_collision_shadowing_lowering_object_pointer_type_sites = 0;
+  std::size_t namespace_collision_shadowing_lowering_pointer_declarator_sites =
+      0;
+  std::size_t namespace_collision_shadowing_lowering_normalized_sites = 0;
+  std::size_t namespace_collision_shadowing_lowering_contract_violation_sites =
+      0;
+  bool deterministic_namespace_collision_shadowing_lowering_handoff = false;
+  std::string lowering_public_private_api_partition_replay_key;
+  std::size_t public_private_api_partition_lowering_sites = 0;
+  std::size_t public_private_api_partition_lowering_namespace_segment_sites = 0;
+  std::size_t
+      public_private_api_partition_lowering_import_edge_candidate_sites = 0;
+  std::size_t public_private_api_partition_lowering_object_pointer_type_sites =
+      0;
+  std::size_t public_private_api_partition_lowering_pointer_declarator_sites =
+      0;
+  std::size_t public_private_api_partition_lowering_normalized_sites = 0;
+  std::size_t public_private_api_partition_lowering_contract_violation_sites =
+      0;
+  bool deterministic_public_private_api_partition_lowering_handoff = false;
+};
--- native/objc3c/src/ir/objc3_ir_frontend_metadata_module_source_linkage.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_module_source_linkage.h
@@ -3,34 +3,10 @@
 #include <cstddef>
 #include <string>
 
-struct Objc3IRFrontendModuleSourceLinkageMetadata {
-  std::string lowering_module_import_graph_replay_key;
-  std::size_t module_import_graph_lowering_sites = 0;
-  std::size_t module_import_graph_lowering_import_edge_candidate_sites = 0;
-  std::size_t module_import_graph_lowering_namespace_segment_sites = 0;
-  std::size_t module_import_graph_lowering_object_pointer_type_sites = 0;
-  std::size_t module_import_graph_lowering_pointer_declarator_sites = 0;
-  std::size_t module_import_graph_lowering_normalized_sites = 0;
-  std::size_t module_import_graph_lowering_contract_violation_sites = 0;
-  bool deterministic_module_import_graph_lowering_handoff = false;
-  std::string lowering_namespace_collision_shadowing_replay_key;
-  std::size_t namespace_collision_shadowing_lowering_sites = 0;
-  std::size_t namespace_collision_shadowing_lowering_namespace_segment_sites = 0;
-  std::size_t namespace_collision_shadowing_lowering_import_edge_candidate_sites = 0;
-  std::size_t namespace_collision_shadowing_lowering_object_pointer_type_sites = 0;
-  std::size_t namespace_collision_shadowing_lowering_pointer_declarator_sites = 0;
-  std::size_t namespace_collision_shadowing_lowering_normalized_sites = 0;
-  std::size_t namespace_collision_shadowing_lowering_contract_violation_sites = 0;
-  bool deterministic_namespace_collision_shadowing_lowering_handoff = false;
-  std::string lowering_public_private_api_partition_replay_key;
-  std::size_t public_private_api_partition_lowering_sites = 0;
-  std::size_t public_private_api_partition_lowering_namespace_segment_sites = 0;
-  std::size_t public_private_api_partition_lowering_import_edge_candidate_sites = 0;
-  std::size_t public_private_api_partition_lowering_object_pointer_type_sites = 0;
-  std::size_t public_private_api_partition_lowering_pointer_declarator_sites = 0;
-  std::size_t public_private_api_partition_lowering_normalized_sites = 0;
-  std::size_t public_private_api_partition_lowering_contract_violation_sites = 0;
-  bool deterministic_public_private_api_partition_lowering_handoff = false;
+#include "ir/objc3_ir_frontend_metadata_module_import_graph.h"
+
+struct Objc3IRFrontendModuleSourceLinkageMetadata
+    : Objc3IRFrontendModuleImportGraphMetadata {
   std::string lowering_incremental_module_cache_invalidation_replay_key;
   std::size_t incremental_module_cache_invalidation_lowering_sites = 0;
   std::size_t incremental_module_cache_invalidation_lowering_namespace_segment_sites = 0;
