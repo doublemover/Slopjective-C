9d75cadd34578efff59502c556d6ed88a0591236
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 00:53:25 -0400

    refactor(json): split array writer scalar wrappers

--- native/objc3c/src/io/json/json_array_writer.cpp
+++ native/objc3c/src/io/json/json_array_writer.cpp
@@ -7,37 +7,6 @@
 
 namespace objc3::io::json {
 
-void JsonArrayWriter::StringValue(std::string_view value) {
-  BeginElement();
-  WriteJsonArrayStringValue(out_, value);
-}
-
-void JsonArrayWriter::BoolValue(bool value) {
-  BeginElement();
-  WriteJsonArrayBoolValue(out_, value);
-}
-
-void JsonArrayWriter::IntValue(std::int64_t value) {
-  BeginElement();
-  WriteJsonArrayIntValue(out_, value);
-}
-
-void JsonArrayWriter::NumberValue(double value) {
-  ValidateJsonArrayNumberValue(value);
-  BeginElement();
-  WriteJsonArrayNumberValue(out_, value);
-}
-
-void JsonArrayWriter::SizeValue(std::size_t value) {
-  BeginElement();
-  WriteJsonArraySizeValue(out_, value);
-}
-
-void JsonArrayWriter::UnsignedValue(std::uint64_t value) {
-  BeginElement();
-  WriteJsonArrayUnsignedValue(out_, value);
-}
-
 void JsonArrayWriter::Value(const JsonValue &value) {
   BeginElement();
   WriteJsonArrayJsonValue(out_, value);
