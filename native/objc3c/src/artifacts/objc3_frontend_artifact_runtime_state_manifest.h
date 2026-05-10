5ca42b5013d3ce68e0a111e7b99b3539c9a7f5cc
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-09 21:08:37 -0400

    refactor(artifacts): split runtime bootstrap lowering manifest

--- native/objc3c/src/artifacts/objc3_frontend_artifact_runtime_state_manifest.h
+++ native/objc3c/src/artifacts/objc3_frontend_artifact_runtime_state_manifest.h
@@ -32,4 +32,14 @@ void WriteRuntimeBootstrapRegistrationSourceSurface(
     const Objc3RuntimeBootstrapLegalitySemanticsSummary
         &runtime_bootstrap_legality_semantics);
 
+void WriteRuntimeBootstrapLoweringRegistrationArtifactSurface(
+    std::ostream &manifest,
+    const std::string &runtime_state_publication_emit_prefix,
+    const Objc3RuntimeTranslationUnitRegistrationManifestSummary
+        &runtime_translation_unit_registration_manifest,
+    const Objc3RuntimeRegistrationDescriptorFrontendClosureSummary
+        &runtime_registration_descriptor_frontend_closure,
+    const Objc3RuntimeBootstrapLoweringSummary &runtime_bootstrap_lowering,
+    const Objc3RuntimeBootstrapSemanticsSummary &runtime_bootstrap_semantics);
+
 }  // namespace objc3::artifacts::frontend
--- native/objc3c/src/artifacts/objc3_frontend_artifacts.cpp
+++ native/objc3c/src/artifacts/objc3_frontend_artifacts.cpp
@@ -6717,86 +6717,12 @@ Objc3FrontendArtifactBundle BuildObjc3FrontendArtifacts(const std::filesystem::p
           runtime_registration_descriptor_frontend_closure,
           runtime_registration_descriptor_image_root_source_surface,
           runtime_bootstrap_lowering, runtime_bootstrap_legality_semantics);
-  manifest << "  \"runtime_bootstrap_lowering_registration_artifact_surface\":{\"contract_id\":\""
-           << kObjc3RuntimeBootstrapLoweringRegistrationArtifactSurfaceContractId
-           << "\",\"compile_manifest_artifact\":\""
-           << runtime_state_publication_emit_prefix << ".manifest.json"
-           << "\",\"registration_manifest_artifact\":\""
-           << runtime_translation_unit_registration_manifest
-                  .manifest_artifact_relative_path
-           << "\",\"registration_descriptor_artifact\":\""
-           << runtime_registration_descriptor_frontend_closure.artifact_relative_path
-           << "\",\"object_artifact\":\""
-           << runtime_state_publication_emit_prefix << ".obj"
-           << "\",\"backend_artifact\":\""
-           << runtime_state_publication_emit_prefix << ".ll"
-           << "\",\"bootstrap_lowering_contract_id\":\""
-           << runtime_bootstrap_lowering.contract_id
-           << "\",\"registration_manifest_contract_id\":\""
-           << runtime_translation_unit_registration_manifest.contract_id
-           << "\",\"bootstrap_semantics_contract_id\":\""
-           << runtime_bootstrap_semantics.contract_id
-           << "\",\"registration_descriptor_frontend_closure_contract_id\":\""
-           << runtime_registration_descriptor_frontend_closure.contract_id
-           << "\",\"runtime_support_library_archive_relative_path\":\""
-           << runtime_translation_unit_registration_manifest
-                  .runtime_support_library_archive_relative_path
-           << "\",\"constructor_root_symbol\":\""
-           << runtime_bootstrap_lowering.constructor_root_symbol
-           << "\",\"init_stub_symbol_prefix\":\""
-           << runtime_bootstrap_lowering.constructor_init_stub_symbol_prefix
-           << "\",\"registration_table_symbol_prefix\":\""
-           << runtime_bootstrap_lowering.registration_table_symbol_prefix
-           << "\",\"image_local_init_state_symbol_prefix\":\""
-           << runtime_bootstrap_lowering.image_local_init_state_symbol_prefix
-           << "\",\"registration_entrypoint_symbol\":\""
-           << runtime_bootstrap_lowering.registration_entrypoint_symbol
-           << "\",\"global_ctor_list_model\":\""
-           << runtime_bootstrap_lowering.global_ctor_list_model
-           << "\",\"registration_table_layout_model\":\""
-           << runtime_bootstrap_lowering.registration_table_layout_model
-           << "\",\"image_local_initialization_model\":\""
-           << runtime_bootstrap_lowering.image_local_initialization_model
-           << "\",\"registration_table_abi_version\":"
-           << runtime_bootstrap_lowering.registration_table_abi_version
-           << ",\"registration_table_pointer_field_count\":"
-           << runtime_bootstrap_lowering
-                  .registration_table_pointer_field_count
-           << ",\"constructor_root_emission_state\":\""
-           << runtime_bootstrap_lowering.constructor_root_emission_state
-           << "\",\"init_stub_emission_state\":\""
-           << runtime_bootstrap_lowering.init_stub_emission_state
-           << "\",\"registration_table_emission_state\":\""
-           << runtime_bootstrap_lowering.registration_table_emission_state
-           << "\",\"lowered_registration_descriptor_fields\":["
-           << "\"constructor_init_stub_symbol\","
-           << "\"bootstrap_registration_table_symbol\","
-           << "\"bootstrap_image_local_init_state_symbol\","
-           << "\"bootstrap_registration_table_layout_model\","
-           << "\"bootstrap_image_local_initialization_model\","
-           << "\"bootstrap_registration_table_abi_version\","
-           << "\"bootstrap_registration_table_pointer_field_count\","
-           << "\"translation_unit_registration_order_ordinal\""
-           << "],\"loader_table_ir_proof_fields\":["
-           << "\"constructor_root_symbol\","
-           << "\"constructor_init_stub_symbol\","
-           << "\"bootstrap_registration_table_symbol\","
-           << "\"bootstrap_image_local_init_state_symbol\","
-           << "\"translation_unit_registration_order_ordinal\""
-           << "]"
-           << ",\"bootstrap_ir_materialization_landed\":"
-           << (runtime_bootstrap_lowering.bootstrap_ir_materialization_landed
-                   ? "true"
-                   : "false")
-           << ",\"image_local_initialization_landed\":"
-           << (runtime_bootstrap_lowering.image_local_initialization_landed
-                   ? "true"
-                   : "false")
-           << ",\"requires_coupled_registration_descriptor_artifact\":true"
-           << ",\"requires_coupled_registration_manifest\":true"
-           << ",\"requires_real_compile_output\":true"
-           << ",\"requires_emitted_loader_table_ir\":true"
-           << "},\n";
+  objc3::artifacts::frontend::
+      WriteRuntimeBootstrapLoweringRegistrationArtifactSurface(
+          manifest, runtime_state_publication_emit_prefix,
+          runtime_translation_unit_registration_manifest,
+          runtime_registration_descriptor_frontend_closure,
+          runtime_bootstrap_lowering, runtime_bootstrap_semantics);
   manifest << "  \"runtime_multi_image_startup_ordering_source_surface\":{\"contract_id\":\""
            << kObjc3RuntimeMultiImageStartupOrderingSourceSurfaceContractId
            << "\",\"compile_manifest_artifact\":\""
