b363a265f044648197ebf42a44aabc6dae087363
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 15:52:24 -0400

    refactor(parse): split nserror bridging profiles

--- native/objc3c/src/parse/objc3_parser_ns_error_bridging_profiles.cpp
+++ native/objc3c/src/parse/objc3_parser_ns_error_bridging_profiles.cpp
@@ -10,86 +10,18 @@
 namespace objc3c::parse {
 namespace {
 
-#include "parse/objc3_parser_ns_error_parameter_profile_building.inc"
+#include "parse/objc3_parser_ns_error_bridging_profiles_site_count_profile_construction.inc"
 
 #include "parse/objc3_parser_ns_error_failable_call_sites.inc"
 
 }  // namespace
 
-std::string BuildNSErrorBridgingProfile(
-    std::size_t ns_error_bridging_sites,
-    std::size_t ns_error_parameter_sites,
-    std::size_t ns_error_out_parameter_sites,
-    std::size_t ns_error_bridge_path_sites,
-    std::size_t failable_call_sites,
-    std::size_t normalized_sites,
-    std::size_t bridge_boundary_sites,
-    std::size_t contract_violation_sites,
-    bool deterministic_ns_error_bridging_lowering_handoff) {
-  std::ostringstream out;
-  out << "ns-error-bridging:ns_error_bridging_sites="
-      << ns_error_bridging_sites
-      << ";ns_error_parameter_sites=" << ns_error_parameter_sites
-      << ";ns_error_out_parameter_sites=" << ns_error_out_parameter_sites
-      << ";ns_error_bridge_path_sites=" << ns_error_bridge_path_sites
-      << ";failable_call_sites=" << failable_call_sites
-      << ";normalized_sites=" << normalized_sites
-      << ";bridge_boundary_sites=" << bridge_boundary_sites
-      << ";contract_violation_sites=" << contract_violation_sites
-      << ";deterministic_ns_error_bridging_lowering_handoff="
-      << (deterministic_ns_error_bridging_lowering_handoff ? "true" : "false");
-  return out.str();
-}
+#include "parse/objc3_parser_ns_error_bridging_profiles_string_serialization.inc"
 
-bool IsNSErrorBridgingProfileNormalized(
-    std::size_t ns_error_bridging_sites,
-    std::size_t ns_error_parameter_sites,
-    std::size_t ns_error_out_parameter_sites,
-    std::size_t ns_error_bridge_path_sites,
-    std::size_t failable_call_sites,
-    std::size_t normalized_sites,
-    std::size_t bridge_boundary_sites,
-    std::size_t contract_violation_sites) {
-  if (ns_error_out_parameter_sites > ns_error_parameter_sites) {
-    return false;
-  }
-  if (ns_error_bridge_path_sites > ns_error_out_parameter_sites ||
-      ns_error_bridge_path_sites > failable_call_sites) {
-    return false;
-  }
-  if (normalized_sites + bridge_boundary_sites != ns_error_bridging_sites) {
-    return false;
-  }
-  if (ns_error_parameter_sites > ns_error_bridging_sites ||
-      ns_error_out_parameter_sites > ns_error_bridging_sites ||
-      ns_error_bridge_path_sites > ns_error_bridging_sites ||
-      failable_call_sites > ns_error_bridging_sites ||
-      normalized_sites > ns_error_bridging_sites ||
-      bridge_boundary_sites > ns_error_bridging_sites) {
-    return false;
-  }
-  return contract_violation_sites == 0;
-}
+#include "parse/objc3_parser_ns_error_bridging_profiles_normalization_predicate.inc"
 
-Objc3NSErrorBridgingProfile BuildNSErrorBridgingProfileFromFunction(
-    const FunctionDecl &fn) {
-  return BuildNSErrorBridgingProfileFromParameters(
-      fn.params, CountFailableCallSitesInBody(fn.body));
-}
+#include "parse/objc3_parser_ns_error_bridging_profiles_function_profile_construction.inc"
 
-Objc3NSErrorBridgingProfile BuildNSErrorBridgingProfileFromOpaqueBody(
-    const Objc3MethodDecl &method) {
-  std::size_t raw_failable_call_sites = 0;
-  if (method.has_body) {
-    for (const auto &param : method.params) {
-      if (IsNSErrorOutParameterSite(param)) {
-        raw_failable_call_sites = 1u;
-        break;
-      }
-    }
-  }
-  return BuildNSErrorBridgingProfileFromParameters(method.params,
-                                                   raw_failable_call_sites);
-}
+#include "parse/objc3_parser_ns_error_bridging_profiles_opaque_method_profile_construction.inc"
 
 }  // namespace objc3c::parse
