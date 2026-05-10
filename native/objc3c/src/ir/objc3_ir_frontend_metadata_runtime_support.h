ffa622d69873291b33d83b7da6436008b1db142c
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 03:17:50 -0400

    HC split IR runtime support evidence metadata

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_runtime_support.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_runtime_support.h
@@ -3,42 +3,9 @@
 #include <cstddef>
 #include <string>
 
+#include "ir/objc3_ir_frontend_metadata_runtime_support_evidence.h"
 #include "ir/objc3_ir_frontend_metadata_runtime_support_library.h"
 
 struct Objc3IRFrontendRuntimeSupportMetadata
-    : Objc3IRFrontendRuntimeSupportLibraryMetadata {
-  std::string runtime_metadata_object_inspection_contract_id;
-  std::string runtime_metadata_object_inspection_publication_contract_id;
-  bool runtime_metadata_object_inspection_matrix_published = false;
-  bool runtime_metadata_object_inspection_fail_closed = false;
-  bool runtime_metadata_object_inspection_uses_llvm_readobj = false;
-  bool runtime_metadata_object_inspection_uses_llvm_objdump = false;
-  std::size_t runtime_metadata_object_inspection_matrix_row_count = 0;
-  std::string runtime_metadata_object_inspection_fixture_path;
-  std::string runtime_metadata_object_inspection_emit_prefix;
-  std::string runtime_metadata_object_inspection_object_relative_path;
-  std::string runtime_metadata_object_inspection_section_inventory_row_key;
-  std::string runtime_metadata_object_inspection_section_inventory_command;
-  std::string runtime_metadata_object_inspection_symbol_inventory_row_key;
-  std::string runtime_metadata_object_inspection_symbol_inventory_command;
-  std::string executable_metadata_debug_projection_contract_id;
-  std::string executable_metadata_debug_projection_typed_handoff_contract_id;
-  std::string executable_metadata_debug_projection_source_graph_contract_id;
-  std::string executable_metadata_debug_projection_named_metadata_name;
-  std::string executable_metadata_debug_projection_manifest_surface_path;
-  std::string executable_metadata_debug_projection_typed_handoff_surface_path;
-  std::string executable_metadata_debug_projection_source_graph_surface_path;
-  bool executable_metadata_debug_projection_matrix_published = false;
-  bool executable_metadata_debug_projection_fail_closed = false;
-  bool executable_metadata_debug_projection_manifest_debug_surface_published =
-      false;
-  bool executable_metadata_debug_projection_ir_named_metadata_published = false;
-  bool executable_metadata_debug_projection_replay_anchor_deterministic = false;
-  bool executable_metadata_debug_projection_active_typed_handoff_ready = false;
-  std::size_t executable_metadata_debug_projection_matrix_row_count = 0;
-  std::string executable_metadata_debug_projection_replay_key;
-  std::string executable_metadata_debug_projection_active_typed_handoff_replay_key;
-  std::string executable_metadata_debug_projection_row0_descriptor;
-  std::string executable_metadata_debug_projection_row1_descriptor;
-  std::string executable_metadata_debug_projection_row2_descriptor;
-};
+    : Objc3IRFrontendRuntimeSupportEvidenceMetadata,
+      Objc3IRFrontendRuntimeSupportLibraryMetadata {};
