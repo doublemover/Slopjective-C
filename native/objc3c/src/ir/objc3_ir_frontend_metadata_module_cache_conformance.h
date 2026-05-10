658813bcec21f4259913f5e5d30aa62d23eb4291
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 03:45:40 -0400

    HC split IR cross-module conformance metadata

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_module_cache_conformance.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_module_cache_conformance.h
@@ -1,21 +1,8 @@
 #pragma once
 
-#include <cstddef>
-#include <string>
-
+#include "ir/objc3_ir_frontend_metadata_cross_module_conformance.h"
 #include "ir/objc3_ir_frontend_metadata_module_cache_invalidation.h"
 
 struct Objc3IRFrontendModuleCacheConformanceMetadata
-    : Objc3IRFrontendModuleCacheInvalidationMetadata {
-  std::string lowering_cross_module_conformance_replay_key;
-  std::size_t cross_module_conformance_lowering_sites = 0;
-  std::size_t cross_module_conformance_lowering_namespace_segment_sites = 0;
-  std::size_t cross_module_conformance_lowering_import_edge_candidate_sites = 0;
-  std::size_t cross_module_conformance_lowering_object_pointer_type_sites = 0;
-  std::size_t cross_module_conformance_lowering_pointer_declarator_sites = 0;
-  std::size_t cross_module_conformance_lowering_normalized_sites = 0;
-  std::size_t
-      cross_module_conformance_lowering_cache_invalidation_candidate_sites = 0;
-  std::size_t cross_module_conformance_lowering_contract_violation_sites = 0;
-  bool deterministic_cross_module_conformance_lowering_handoff = false;
-};
+    : Objc3IRFrontendModuleCacheInvalidationMetadata,
+      Objc3IRFrontendCrossModuleConformanceMetadata {};
