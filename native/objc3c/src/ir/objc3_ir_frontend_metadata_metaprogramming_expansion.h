f5eaf1b2a55f07eaf933908bbf003a10dcc1e105
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 04:33:58 -0400

    HC split IR metaprogramming expansion lowering guard metadata

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_expansion.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_expansion.h
@@ -1,17 +1,10 @@
 #pragma once
 
-#include <cstddef>
-
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_expansion_lowering_guards.h"
 #include "ir/objc3_ir_frontend_metadata_metaprogramming_expansion_lowering_sites.h"
 #include "ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission.h"
 
-struct Objc3IRFrontendMetaprogrammingExpansionLoweringMetadata {
-  std::size_t metaprogramming_expansion_lowering_guard_blocked_sites = 0;
-  std::size_t metaprogramming_expansion_lowering_contract_violation_sites = 0;
-  bool deterministic_metaprogramming_expansion_lowering_handoff = false;
-};
-
 struct Objc3IRFrontendMetaprogrammingExpansionMetadata
     : Objc3IRFrontendMetaprogrammingExpansionLoweringSitesMetadata,
-      Objc3IRFrontendMetaprogrammingExpansionLoweringMetadata,
+      Objc3IRFrontendMetaprogrammingExpansionLoweringGuardsMetadata,
       Objc3IRFrontendMetaprogrammingSynthesizedEmissionMetadata {};
