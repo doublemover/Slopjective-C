4407a4615dc096c739d185d3220193dccff0add2
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 04:14:22 -0400

    HC split IR metaprogramming synthesized emission metadata

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_expansion.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_expansion.h
@@ -2,11 +2,10 @@
 
 #include <cstddef>
 #include <string>
-#include <vector>
 
-#include "ir/objc3_ir_frontend_metadata_metaprogramming_bundles.h"
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission.h"
 
-struct Objc3IRFrontendMetaprogrammingExpansionMetadata {
+struct Objc3IRFrontendMetaprogrammingExpansionLoweringMetadata {
   std::string lowering_metaprogramming_expansion_replay_key;
   std::size_t metaprogramming_expansion_lowering_derive_inventory_sites = 0;
   std::size_t metaprogramming_expansion_lowering_derived_selector_artifact_sites = 0;
@@ -19,19 +18,8 @@ struct Objc3IRFrontendMetaprogrammingExpansionMetadata {
   std::size_t metaprogramming_expansion_lowering_guard_blocked_sites = 0;
   std::size_t metaprogramming_expansion_lowering_contract_violation_sites = 0;
   bool deterministic_metaprogramming_expansion_lowering_handoff = false;
-  std::string lowering_metaprogramming_synthesized_emission_replay_key;
-  std::size_t metaprogramming_synthesized_emitted_derive_method_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_macro_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_property_behavior_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_global_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_runtime_method_list_sites = 0;
-  std::size_t metaprogramming_synthesized_guard_blocked_sites = 0;
-  std::size_t metaprogramming_synthesized_contract_violation_sites = 0;
-  bool deterministic_metaprogramming_synthesized_emission_handoff = false;
-  std::vector<Objc3IRMetaprogrammingDerivedMethodBundle>
-      metaprogramming_derived_method_bundles_lexicographic;
-  std::vector<Objc3IRMetaprogrammingMacroArtifactBundle>
-      metaprogramming_macro_artifact_bundles_lexicographic;
-  std::vector<Objc3IRMetaprogrammingPropertyBehaviorArtifactBundle>
-      metaprogramming_property_behavior_artifact_bundles_lexicographic;
 };
+
+struct Objc3IRFrontendMetaprogrammingExpansionMetadata
+    : Objc3IRFrontendMetaprogrammingExpansionLoweringMetadata,
+      Objc3IRFrontendMetaprogrammingSynthesizedEmissionMetadata {};
---
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+#include <vector>
+
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_bundles.h"
+
+struct Objc3IRFrontendMetaprogrammingSynthesizedEmissionMetadata {
+  std::string lowering_metaprogramming_synthesized_emission_replay_key;
+  std::size_t metaprogramming_synthesized_emitted_derive_method_sites = 0;
+  std::size_t metaprogramming_synthesized_emitted_macro_artifact_sites = 0;
+  std::size_t metaprogramming_synthesized_emitted_property_behavior_artifact_sites = 0;
+  std::size_t metaprogramming_synthesized_emitted_global_artifact_sites = 0;
+  std::size_t metaprogramming_synthesized_emitted_runtime_method_list_sites = 0;
+  std::size_t metaprogramming_synthesized_guard_blocked_sites = 0;
+  std::size_t metaprogramming_synthesized_contract_violation_sites = 0;
+  bool deterministic_metaprogramming_synthesized_emission_handoff = false;
+  std::vector<Objc3IRMetaprogrammingDerivedMethodBundle>
+      metaprogramming_derived_method_bundles_lexicographic;
+  std::vector<Objc3IRMetaprogrammingMacroArtifactBundle>
+      metaprogramming_macro_artifact_bundles_lexicographic;
+  std::vector<Objc3IRMetaprogrammingPropertyBehaviorArtifactBundle>
+      metaprogramming_property_behavior_artifact_bundles_lexicographic;
+};
