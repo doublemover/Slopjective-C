7202e438dfa0620eb0ea6c7c6823fe7d524e76d9
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 01:44:19 -0400

    HC split dispatch sequence pass groups

--- native/objc3c/src/tools/objc3c_frontend_c_api_runner_command_parser_dispatch_sequence.cpp
+++ native/objc3c/src/tools/objc3c_frontend_c_api_runner_command_parser_dispatch_sequence.cpp
@@ -1,6 +1,4 @@
-#include "tools/objc3c_frontend_c_api_runner_command_parser_dispatch_internal.h"
-
-#include "tools/objc3c_frontend_c_api_runner_command_parser_internal.h"
+#include "tools/objc3c_frontend_c_api_runner_command_parser_dispatch_sequence_internal.h"
 
 FrontendCApiRunnerCommandOptionParseResult
 DispatchFrontendCApiRunnerCommandOptionPassSequence(
@@ -11,26 +9,28 @@ DispatchFrontendCApiRunnerCommandOptionPassSequence(
     FrontendCApiRunnerOptions &options,
     std::string &error) {
   const FrontendCApiRunnerCommandOptionParseResult path_runtime_result =
-      DispatchFrontendCApiRunnerPathRuntimeOptionPass(arg,
-                                                      argc,
-                                                      argv,
-                                                      index,
-                                                      options,
-                                                      error);
+      DispatchFrontendCApiRunnerCommandOptionPathRuntimePass(arg,
+                                                             argc,
+                                                             argv,
+                                                             index,
+                                                             options,
+                                                             error);
   if (FrontendCApiRunnerDispatchShouldReturn(path_runtime_result)) {
     return path_runtime_result;
   }
 
   const FrontendCApiRunnerCommandOptionParseResult backend_result =
-      DispatchFrontendCApiRunnerBackendOptionPass(arg,
-                                                  argc,
-                                                  argv,
-                                                  index,
-                                                  options,
-                                                  error);
+      DispatchFrontendCApiRunnerCommandOptionBackendPass(arg,
+                                                         argc,
+                                                         argv,
+                                                         index,
+                                                         options,
+                                                         error);
   if (FrontendCApiRunnerDispatchShouldReturn(backend_result)) {
     return backend_result;
   }
 
-  return DispatchFrontendCApiRunnerEmissionHelpOptionPass(arg, options, error);
+  return DispatchFrontendCApiRunnerCommandOptionEmissionHelpPass(arg,
+                                                                options,
+                                                                error);
 }
---
+++ native/objc3c/src/tools/objc3c_frontend_c_api_runner_command_parser_dispatch_sequence_backend.cpp
@@ -0,0 +1,19 @@
+#include "tools/objc3c_frontend_c_api_runner_command_parser_dispatch_sequence_internal.h"
+
+#include "tools/objc3c_frontend_c_api_runner_command_parser_internal.h"
+
+FrontendCApiRunnerCommandOptionParseResult
+DispatchFrontendCApiRunnerCommandOptionBackendPass(
+    const std::string &arg,
+    int argc,
+    char **argv,
+    int &index,
+    FrontendCApiRunnerOptions &options,
+    std::string &error) {
+  return DispatchFrontendCApiRunnerBackendOptionPass(arg,
+                                                     argc,
+                                                     argv,
+                                                     index,
+                                                     options,
+                                                     error);
+}
