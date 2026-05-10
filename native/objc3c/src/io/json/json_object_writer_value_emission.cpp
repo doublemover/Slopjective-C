ff98b80b71e7d0df0d92faa7a72beed362dcba88
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 01:29:13 -0400

    refactor(json): split object writer scalar emission

--- native/objc3c/src/io/json/json_object_writer_value_emission.cpp
+++ native/objc3c/src/io/json/json_object_writer_value_emission.cpp
@@ -1,38 +1,10 @@
 #include "io/json/json_object_writer_value_emission.h"
 
-#include <iomanip>
 #include <ostream>
 
 #include "io/json/json_writer.h"
-#include "io/objc3_json.h"
 
 namespace objc3::io::json {
-
-void WriteJsonObjectStringMemberValue(std::ostream &out,
-                                      std::string_view value) {
-  objc3::io::WriteJsonString(out, value);
-}
-
-void WriteJsonObjectBoolMemberValue(std::ostream &out, bool value) {
-  out << (value ? "true" : "false");
-}
-
-void WriteJsonObjectIntMemberValue(std::ostream &out, std::int64_t value) {
-  out << value;
-}
-
-void WriteJsonObjectNumberMemberValue(std::ostream &out, double value) {
-  out << std::setprecision(17) << value;
-}
-
-void WriteJsonObjectSizeMemberValue(std::ostream &out, std::size_t value) {
-  out << value;
-}
-
-void WriteJsonObjectUnsignedMemberValue(std::ostream &out,
-                                        std::uint64_t value) {
-  out << value;
-}
 
 void WriteJsonObjectStringArrayMemberValue(
     std::ostream &out,
