b276bf58eae7ba7e29d5f82ffecea39edb99a950
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 04:45:01 -0400

    HC split IR metaprogramming synthesized emission site metadata

---
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission_artifact_sites.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+struct Objc3IRFrontendMetaprogrammingSynthesizedEmissionArtifactSitesMetadata {
+  std::string lowering_metaprogramming_synthesized_emission_replay_key;
+  std::size_t metaprogramming_synthesized_emitted_derive_method_sites = 0;
+  std::size_t metaprogramming_synthesized_emitted_macro_artifact_sites = 0;
+  std::size_t metaprogramming_synthesized_emitted_property_behavior_artifact_sites = 0;
+  std::size_t metaprogramming_synthesized_emitted_global_artifact_sites = 0;
+};
--- native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission_sites.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission_sites.h
@@ -1,13 +1,8 @@
 #pragma once
 
-#include <cstddef>
-#include <string>
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission_artifact_sites.h"
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission_runtime_sites.h"
 
-struct Objc3IRFrontendMetaprogrammingSynthesizedEmissionSitesMetadata {
-  std::string lowering_metaprogramming_synthesized_emission_replay_key;
-  std::size_t metaprogramming_synthesized_emitted_derive_method_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_macro_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_property_behavior_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_global_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_runtime_method_list_sites = 0;
-};
+struct Objc3IRFrontendMetaprogrammingSynthesizedEmissionSitesMetadata
+    : Objc3IRFrontendMetaprogrammingSynthesizedEmissionArtifactSitesMetadata,
+      Objc3IRFrontendMetaprogrammingSynthesizedEmissionRuntimeSitesMetadata {};
