2d01d4529ade2f321196e7d4722092a7c29bd9ac
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 00:06:11 -0400

    refactor(json): split value object lookup

--- native/objc3c/src/io/json/json_value_container.cpp
+++ native/objc3c/src/io/json/json_value_container.cpp
@@ -47,28 +47,4 @@ const JsonValue::Object &JsonValue::AsObject() const {
   return IsObject() ? object_value_ : EmptyObject();
 }
 
-const JsonValue *JsonValue::Find(std::string_view key) const {
-  if (!IsObject()) {
-    return nullptr;
-  }
-  const auto found = object_value_.find(std::string(key));
-  return found == object_value_.end() ? nullptr : &found->second;
-}
-
-std::optional<std::string> JsonValue::GetString(std::string_view key) const {
-  const JsonValue *value = Find(key);
-  if (value == nullptr || !value->IsString()) {
-    return std::nullopt;
-  }
-  return value->AsString();
-}
-
-std::optional<bool> JsonValue::GetBool(std::string_view key) const {
-  const JsonValue *value = Find(key);
-  if (value == nullptr || !value->IsBool()) {
-    return std::nullopt;
-  }
-  return value->AsBool();
-}
-
 }  // namespace objc3::io::json
