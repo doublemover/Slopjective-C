e357f604cb310813cfa394313d741043b56641ba
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-09 21:29:23 -0400

    refactor(ir): split module metadata tail publication

--- native/objc3c/src/ir/objc3_ir_module_metadata_publication.cpp
+++ native/objc3c/src/ir/objc3_ir_module_metadata_publication.cpp
@@ -11,14 +11,7 @@
 #include "ir/objc3_ir_module_metadata_publication_lowering_profiles.h"
 #include "ir/objc3_ir_module_metadata_publication_prelude.h"
 #include "ir/objc3_ir_module_metadata_publication_runtime_gates.h"
-
-namespace {
-
-struct Objc3IRModuleMetadataPublicationProgramView {
-  const std::string &module_name;
-};
-
-}  // namespace
+#include "ir/objc3_ir_module_metadata_publication_tail.h"
 
 void EmitObjc3IRModuleMetadataPublication(
     const Objc3IRModuleMetadataPublicationOptions &options,
@@ -31,8 +24,6 @@ void EmitObjc3IRModuleMetadataPublication(
       options.synthesized_property_accessor_count;
   const std::size_t vector_signature_function_count_ =
       options.vector_signature_function_count;
-  const Objc3IRModuleMetadataPublicationProgramView program_{
-      options.module_name};
     EmitObjc3IRModuleMetadataPreludePublication(
         Objc3IRModuleMetadataPreludePublicationOptions{
             frontend_metadata_, lowering_ir_boundary_,
@@ -47,43 +38,6 @@ void EmitObjc3IRModuleMetadataPublication(
         frontend_metadata_, out);
     EmitObjc3IRModuleMetadataLoweringProfilePublication(frontend_metadata_, out);
     EmitObjc3IRModuleMetadataAdvancedProfilePublication(frontend_metadata_, out);
-    out << "; frontend_objc_object_pointer_nullability_generics_profile = object_pointer_type_spellings="
-        << frontend_metadata_.object_pointer_type_spellings
-        << ", pointer_declarator_entries=" << frontend_metadata_.pointer_declarator_entries
-        << ", pointer_declarator_depth_total=" << frontend_metadata_.pointer_declarator_depth_total
-        << ", pointer_declarator_token_entries=" << frontend_metadata_.pointer_declarator_token_entries
-        << ", nullability_suffix_entries=" << frontend_metadata_.nullability_suffix_entries
-        << ", generic_suffix_entries=" << frontend_metadata_.generic_suffix_entries
-        << ", terminated_generic_suffix_entries=" << frontend_metadata_.terminated_generic_suffix_entries
-        << ", unterminated_generic_suffix_entries=" << frontend_metadata_.unterminated_generic_suffix_entries
-        << ", deterministic_object_pointer_nullability_generics_handoff="
-        << (frontend_metadata_.deterministic_object_pointer_nullability_generics_handoff ? "true" : "false") << "\n";
-    out << "; frontend_objc_symbol_graph_scope_resolution_profile = global_symbol_nodes="
-        << frontend_metadata_.global_symbol_nodes
-        << ", function_symbol_nodes=" << frontend_metadata_.function_symbol_nodes
-        << ", interface_symbol_nodes=" << frontend_metadata_.interface_symbol_nodes
-        << ", implementation_symbol_nodes=" << frontend_metadata_.implementation_symbol_nodes
-        << ", interface_property_symbol_nodes=" << frontend_metadata_.interface_property_symbol_nodes
-        << ", implementation_property_symbol_nodes=" << frontend_metadata_.implementation_property_symbol_nodes
-        << ", interface_method_symbol_nodes=" << frontend_metadata_.interface_method_symbol_nodes
-        << ", implementation_method_symbol_nodes=" << frontend_metadata_.implementation_method_symbol_nodes
-        << ", top_level_scope_symbols=" << frontend_metadata_.top_level_scope_symbols
-        << ", nested_scope_symbols=" << frontend_metadata_.nested_scope_symbols
-        << ", scope_frames_total=" << frontend_metadata_.scope_frames_total
-        << ", implementation_interface_resolution_sites="
-        << frontend_metadata_.implementation_interface_resolution_sites
-        << ", implementation_interface_resolution_hits="
-        << frontend_metadata_.implementation_interface_resolution_hits
-        << ", implementation_interface_resolution_misses="
-        << frontend_metadata_.implementation_interface_resolution_misses
-        << ", method_resolution_sites=" << frontend_metadata_.method_resolution_sites
-        << ", method_resolution_hits=" << frontend_metadata_.method_resolution_hits
-        << ", method_resolution_misses=" << frontend_metadata_.method_resolution_misses
-        << ", deterministic_symbol_graph_handoff="
-        << (frontend_metadata_.deterministic_symbol_graph_handoff ? "true" : "false")
-        << ", deterministic_scope_resolution_handoff="
-        << (frontend_metadata_.deterministic_scope_resolution_handoff ? "true" : "false")
-        << ", deterministic_symbol_graph_scope_resolution_handoff_key="
-        << frontend_metadata_.deterministic_symbol_graph_scope_resolution_handoff_key << "\n";
-    out << "source_filename = \"" << program_.module_name << ".objc3\"\n\n";
+    EmitObjc3IRModuleMetadataTailPublication(
+        frontend_metadata_, options.module_name, out);
 }
