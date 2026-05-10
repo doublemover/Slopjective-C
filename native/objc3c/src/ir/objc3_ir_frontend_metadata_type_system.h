018b7d90572232edbb8eb84c5de47d00b9a22419
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 03:50:49 -0400

    HC split IR type constraint metadata

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_type_system.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_type_system.h
@@ -3,25 +3,10 @@
 #include <cstddef>
 #include <string>
 
-struct Objc3IRFrontendTypeSystemMetadata {
-  std::string lowering_lightweight_generic_constraint_replay_key;
-  std::size_t lightweight_generic_constraint_lowering_generic_constraint_sites = 0;
-  std::size_t lightweight_generic_constraint_lowering_generic_suffix_sites = 0;
-  std::size_t lightweight_generic_constraint_lowering_object_pointer_type_sites = 0;
-  std::size_t lightweight_generic_constraint_lowering_terminated_generic_suffix_sites = 0;
-  std::size_t lightweight_generic_constraint_lowering_pointer_declarator_sites = 0;
-  std::size_t lightweight_generic_constraint_lowering_normalized_constraint_sites = 0;
-  std::size_t lightweight_generic_constraint_lowering_contract_violation_sites = 0;
-  bool deterministic_lightweight_generic_constraint_lowering_handoff = false;
-  std::string lowering_nullability_flow_warning_precision_replay_key;
-  std::size_t nullability_flow_warning_precision_lowering_sites = 0;
-  std::size_t nullability_flow_warning_precision_lowering_object_pointer_type_sites = 0;
-  std::size_t nullability_flow_warning_precision_lowering_nullability_suffix_sites = 0;
-  std::size_t nullability_flow_warning_precision_lowering_nullable_suffix_sites = 0;
-  std::size_t nullability_flow_warning_precision_lowering_nonnull_suffix_sites = 0;
-  std::size_t nullability_flow_warning_precision_lowering_normalized_sites = 0;
-  std::size_t nullability_flow_warning_precision_lowering_contract_violation_sites = 0;
-  bool deterministic_nullability_flow_warning_precision_lowering_handoff = false;
+#include "ir/objc3_ir_frontend_metadata_type_constraints.h"
+
+struct Objc3IRFrontendTypeSystemMetadata
+    : Objc3IRFrontendTypeConstraintsMetadata {
   std::string lowering_protocol_qualified_object_type_replay_key;
   std::size_t protocol_qualified_object_type_lowering_sites = 0;
   std::size_t protocol_qualified_object_type_lowering_protocol_composition_sites = 0;
