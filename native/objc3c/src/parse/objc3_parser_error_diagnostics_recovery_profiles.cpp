a2279b2c4d996897fb2e5ea87ea862a1f48747c5
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 15:44:58 -0400

    refactor(parse): split diagnostic recovery profiles

--- native/objc3c/src/parse/objc3_parser_error_diagnostics_recovery_profiles.cpp
+++ native/objc3c/src/parse/objc3_parser_error_diagnostics_recovery_profiles.cpp
@@ -18,84 +18,9 @@ namespace {
 
 }  // namespace
 
-std::string BuildErrorDiagnosticsRecoveryProfile(
-    std::size_t error_diagnostics_recovery_sites,
-    std::size_t diagnostic_emit_sites,
-    std::size_t recovery_anchor_sites,
-    std::size_t recovery_boundary_sites,
-    std::size_t fail_closed_diagnostic_sites,
-    std::size_t normalized_sites,
-    std::size_t gate_blocked_sites,
-    std::size_t contract_violation_sites,
-    bool deterministic_error_diagnostics_recovery_handoff) {
-  std::ostringstream out;
-  out << "error-diagnostics-recovery:error_diagnostics_recovery_sites="
-      << error_diagnostics_recovery_sites
-      << ";diagnostic_emit_sites=" << diagnostic_emit_sites
-      << ";recovery_anchor_sites=" << recovery_anchor_sites
-      << ";recovery_boundary_sites=" << recovery_boundary_sites
-      << ";fail_closed_diagnostic_sites=" << fail_closed_diagnostic_sites
-      << ";normalized_sites=" << normalized_sites
-      << ";gate_blocked_sites=" << gate_blocked_sites
-      << ";contract_violation_sites=" << contract_violation_sites
-      << ";deterministic_error_diagnostics_recovery_handoff="
-      << (deterministic_error_diagnostics_recovery_handoff ? "true"
-                                                           : "false");
-  return out.str();
-}
-
-bool IsErrorDiagnosticsRecoveryProfileNormalized(
-    std::size_t error_diagnostics_recovery_sites,
-    std::size_t diagnostic_emit_sites,
-    std::size_t recovery_anchor_sites,
-    std::size_t recovery_boundary_sites,
-    std::size_t fail_closed_diagnostic_sites,
-    std::size_t normalized_sites,
-    std::size_t gate_blocked_sites,
-    std::size_t contract_violation_sites) {
-  if (diagnostic_emit_sites > error_diagnostics_recovery_sites ||
-      recovery_anchor_sites > error_diagnostics_recovery_sites ||
-      recovery_boundary_sites > error_diagnostics_recovery_sites ||
-      fail_closed_diagnostic_sites > error_diagnostics_recovery_sites ||
-      normalized_sites > error_diagnostics_recovery_sites ||
-      gate_blocked_sites > error_diagnostics_recovery_sites ||
-      contract_violation_sites > error_diagnostics_recovery_sites) {
-    return false;
-  }
-  std::size_t normalized_total = 0u;
-  if (!TryAddErrorDiagnosticsRecoverySiteCounts(
-          normalized_sites, gate_blocked_sites, normalized_total)) {
-    return false;
-  }
-  if (normalized_total != error_diagnostics_recovery_sites) {
-    return false;
-  }
-  return contract_violation_sites == 0u;
-}
-
-Objc3ErrorDiagnosticsRecoveryProfile
-BuildErrorDiagnosticsRecoveryProfileFromFunction(const FunctionDecl &fn) {
-  const Objc3ErrorDiagnosticsRecoverySiteCounts counts =
-      CountErrorDiagnosticsRecoverySitesInBody(fn.body);
-  return BuildErrorDiagnosticsRecoveryProfileFromCounts(
-      counts.diagnostic_emit_sites,
-      counts.recovery_anchor_sites,
-      counts.recovery_boundary_sites,
-      counts.fail_closed_diagnostic_sites);
-}
-
-Objc3ErrorDiagnosticsRecoveryProfile
-BuildErrorDiagnosticsRecoveryProfileFromOpaqueBody(
-    const Objc3MethodDecl &method) {
-  Objc3ErrorDiagnosticsRecoverySiteCounts counts;
-  Objc3ProfileSymbolWalker walker{
-      &CollectErrorDiagnosticsRecoveryProfileSymbol, &counts};
-  WalkObjc3ProfileSymbolsInOpaqueMethodBody(method, walker);
-  return BuildErrorDiagnosticsRecoveryProfileFromCounts(
-      counts.diagnostic_emit_sites,
-      counts.recovery_anchor_sites,
-      counts.recovery_boundary_sites,
-      counts.fail_closed_diagnostic_sites);
-}
+#include "parse/objc3_parser_error_diagnostics_recovery_profiles_string_serialization.inc"
+#include "parse/objc3_parser_error_diagnostics_recovery_profiles_normalization_predicate.inc"
+#include "parse/objc3_parser_error_diagnostics_recovery_profiles_function_profile_construction.inc"
+#include "parse/objc3_parser_error_diagnostics_recovery_profiles_opaque_method_profile_construction.inc"
 
 }  // namespace objc3c::parse
