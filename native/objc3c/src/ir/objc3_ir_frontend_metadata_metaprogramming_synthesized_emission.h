4b10d7b5399bb473bbb4a2f3f6b87d55c86dc94d
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 04:27:49 -0400

    HC split IR metaprogramming synthesized emission sites metadata

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission.h
@@ -1,22 +1,17 @@
 #pragma once
 
 #include <cstddef>
-#include <string>
 
 #include "ir/objc3_ir_frontend_metadata_metaprogramming_artifact_model.h"
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission_sites.h"
 
 struct Objc3IRFrontendMetaprogrammingSynthesizedEmissionCountersMetadata {
-  std::string lowering_metaprogramming_synthesized_emission_replay_key;
-  std::size_t metaprogramming_synthesized_emitted_derive_method_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_macro_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_property_behavior_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_global_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_runtime_method_list_sites = 0;
   std::size_t metaprogramming_synthesized_guard_blocked_sites = 0;
   std::size_t metaprogramming_synthesized_contract_violation_sites = 0;
   bool deterministic_metaprogramming_synthesized_emission_handoff = false;
 };
 
 struct Objc3IRFrontendMetaprogrammingSynthesizedEmissionMetadata
-    : Objc3IRFrontendMetaprogrammingSynthesizedEmissionCountersMetadata,
+    : Objc3IRFrontendMetaprogrammingSynthesizedEmissionSitesMetadata,
+      Objc3IRFrontendMetaprogrammingSynthesizedEmissionCountersMetadata,
       Objc3IRFrontendMetaprogrammingArtifactModelMetadata {};
---
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_synthesized_emission_sites.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstddef>
+#include <string>
+
+struct Objc3IRFrontendMetaprogrammingSynthesizedEmissionSitesMetadata {
+  std::string lowering_metaprogramming_synthesized_emission_replay_key;
+  std::size_t metaprogramming_synthesized_emitted_derive_method_sites = 0;
+  std::size_t metaprogramming_synthesized_emitted_macro_artifact_sites = 0;
+  std::size_t metaprogramming_synthesized_emitted_property_behavior_artifact_sites = 0;
+  std::size_t metaprogramming_synthesized_emitted_global_artifact_sites = 0;
+  std::size_t metaprogramming_synthesized_emitted_runtime_method_list_sites = 0;
+};
