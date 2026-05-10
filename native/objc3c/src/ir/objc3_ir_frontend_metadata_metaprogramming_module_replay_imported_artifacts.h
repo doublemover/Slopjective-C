a1d2bffafc7c418283e97596442104405755ce51
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 04:40:42 -0400

    HC split IR metaprogramming replay import count metadata

---
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_imported_artifacts.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#include <cstddef>
+
+struct Objc3IRFrontendMetaprogrammingModuleReplayImportedArtifactsMetadata {
+  std::size_t metaprogramming_module_replay_imported_module_count = 0;
+  std::size_t metaprogramming_module_replay_imported_derive_method_count = 0;
+  std::size_t metaprogramming_module_replay_imported_macro_artifact_count = 0;
+};
--- native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_imports.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_imports.h
@@ -1,21 +1,14 @@
 #pragma once
 
-#include <cstddef>
-
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_imported_artifacts.h"
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_imported_property_behavior.h"
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_imported_runtime_methods.h"
 #include "ir/objc3_ir_frontend_metadata_metaprogramming_module_replay_readiness.h"
 
-struct Objc3IRFrontendMetaprogrammingModuleReplayImportCountsMetadata {
-  std::size_t metaprogramming_module_replay_imported_module_count = 0;
-  std::size_t metaprogramming_module_replay_imported_derive_method_count = 0;
-  std::size_t metaprogramming_module_replay_imported_macro_artifact_count = 0;
-  std::size_t
-      metaprogramming_module_replay_imported_interface_property_behavior_artifact_count =
-          0;
-  std::size_t
-      metaprogramming_module_replay_imported_implementation_property_behavior_artifact_count =
-          0;
-  std::size_t metaprogramming_module_replay_imported_runtime_method_list_count = 0;
-};
+struct Objc3IRFrontendMetaprogrammingModuleReplayImportCountsMetadata
+    : Objc3IRFrontendMetaprogrammingModuleReplayImportedArtifactsMetadata,
+      Objc3IRFrontendMetaprogrammingModuleReplayImportedPropertyBehaviorMetadata,
+      Objc3IRFrontendMetaprogrammingModuleReplayImportedRuntimeMethodsMetadata {};
 
 struct Objc3IRFrontendMetaprogrammingModuleReplayImportsMetadata
     : Objc3IRFrontendMetaprogrammingModuleReplayImportCountsMetadata,
