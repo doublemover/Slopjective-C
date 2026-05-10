15b63d9e2e700bdec5d6767fceb8fa1b24934dc2
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 04:08:45 -0400

    HC split IR metaprogramming expansion metadata

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming.h
@@ -2,38 +2,11 @@
 
 #include <cstddef>
 #include <string>
-#include <vector>
 
-#include "ir/objc3_ir_frontend_metadata_metaprogramming_bundles.h"
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_expansion.h"
 
-struct Objc3IRFrontendMetaprogrammingMetadata {
-  std::string lowering_metaprogramming_expansion_replay_key;
-  std::size_t metaprogramming_expansion_lowering_derive_inventory_sites = 0;
-  std::size_t metaprogramming_expansion_lowering_derived_selector_artifact_sites = 0;
-  std::size_t metaprogramming_expansion_lowering_macro_replay_visible_sites = 0;
-  std::size_t metaprogramming_expansion_lowering_property_behavior_sites = 0;
-  std::size_t metaprogramming_expansion_lowering_synthesized_binding_sites = 0;
-  std::size_t metaprogramming_expansion_lowering_synthesized_getter_sites = 0;
-  std::size_t metaprogramming_expansion_lowering_synthesized_setter_sites = 0;
-  std::size_t metaprogramming_expansion_lowering_replay_visible_metadata_sites = 0;
-  std::size_t metaprogramming_expansion_lowering_guard_blocked_sites = 0;
-  std::size_t metaprogramming_expansion_lowering_contract_violation_sites = 0;
-  bool deterministic_metaprogramming_expansion_lowering_handoff = false;
-  std::string lowering_metaprogramming_synthesized_emission_replay_key;
-  std::size_t metaprogramming_synthesized_emitted_derive_method_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_macro_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_property_behavior_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_global_artifact_sites = 0;
-  std::size_t metaprogramming_synthesized_emitted_runtime_method_list_sites = 0;
-  std::size_t metaprogramming_synthesized_guard_blocked_sites = 0;
-  std::size_t metaprogramming_synthesized_contract_violation_sites = 0;
-  bool deterministic_metaprogramming_synthesized_emission_handoff = false;
-  std::vector<Objc3IRMetaprogrammingDerivedMethodBundle>
-      metaprogramming_derived_method_bundles_lexicographic;
-  std::vector<Objc3IRMetaprogrammingMacroArtifactBundle>
-      metaprogramming_macro_artifact_bundles_lexicographic;
-  std::vector<Objc3IRMetaprogrammingPropertyBehaviorArtifactBundle>
-      metaprogramming_property_behavior_artifact_bundles_lexicographic;
+struct Objc3IRFrontendMetaprogrammingMetadata
+    : Objc3IRFrontendMetaprogrammingExpansionMetadata {
   std::string lowering_metaprogramming_module_interface_replay_preservation_key;
   std::size_t metaprogramming_module_replay_local_derive_method_count = 0;
   std::size_t metaprogramming_module_replay_local_macro_artifact_count = 0;
