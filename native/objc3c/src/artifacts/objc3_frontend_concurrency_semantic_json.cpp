96a9eda273f95658d81308b82e7876cdee39170d
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 14:19:58 -0400

    HC align concurrency semantic JSON writer fragments

--- native/objc3c/src/artifacts/objc3_frontend_concurrency_semantic_json.cpp
+++ native/objc3c/src/artifacts/objc3_frontend_concurrency_semantic_json.cpp
@@ -13,11 +13,11 @@ using objc3::io::EscapeJsonString;
 
 }  // namespace
 
-#include "artifacts/objc3_frontend_concurrency_semantic_json_task_executor_cancellation.inc"
-#include "artifacts/objc3_frontend_concurrency_semantic_json_async_effect_suspension.inc"
-#include "artifacts/objc3_frontend_concurrency_semantic_json_await_suspension_resume.inc"
-#include "artifacts/objc3_frontend_concurrency_semantic_json_async_diagnostics.inc"
-#include "artifacts/objc3_frontend_concurrency_semantic_json_structured_task_cancellation.inc"
-#include "artifacts/objc3_frontend_concurrency_semantic_json_executor_hop_affinity.inc"
+#include "artifacts/objc3_frontend_concurrency_semantic_json_writer_task_executor_cancellation.inc"
+#include "artifacts/objc3_frontend_concurrency_semantic_json_writer_async_effect_suspension.inc"
+#include "artifacts/objc3_frontend_concurrency_semantic_json_writer_await_suspension_resume.inc"
+#include "artifacts/objc3_frontend_concurrency_semantic_json_writer_async_diagnostics.inc"
+#include "artifacts/objc3_frontend_concurrency_semantic_json_writer_structured_task_cancellation.inc"
+#include "artifacts/objc3_frontend_concurrency_semantic_json_writer_executor_hop_affinity.inc"
 
 }  // namespace objc3::artifacts::frontend
