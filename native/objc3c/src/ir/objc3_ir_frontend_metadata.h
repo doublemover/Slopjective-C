96353c626efb1225aad6c5fd56c346f86009ed98
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 02:42:39 -0400

    HC split IR dispatch support metadata

--- native/objc3c/src/ir/objc3_ir_frontend_metadata.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata.h
@@ -9,6 +9,7 @@
 #include "ir/objc3_ir_frontend_metadata_block.h"
 #include "ir/objc3_ir_frontend_metadata_concurrency.h"
 #include "ir/objc3_ir_frontend_metadata_dispatch.h"
+#include "ir/objc3_ir_frontend_metadata_dispatch_support.h"
 #include "ir/objc3_ir_frontend_metadata_error_handling.h"
 #include "ir/objc3_ir_frontend_metadata_metaprogramming_bundles.h"
 #include "ir/objc3_ir_frontend_metadata_module_source_linkage.h"
@@ -34,7 +35,8 @@ struct Objc3IRFrontendMetadata : Objc3IRFrontendRuntimeSupportMetadata,
                                  Objc3IRFrontendModuleSourceLinkageMetadata,
                                  Objc3IRFrontendErrorHandlingMetadata,
                                  Objc3IRFrontendSemanticSurfaceMetadata,
-                                 Objc3IRFrontendConcurrencyMetadata {
+                                 Objc3IRFrontendConcurrencyMetadata,
+                                 Objc3IRFrontendDispatchSupportMetadata {
   std::uint8_t language_version = 3u;
   std::string language_profile = "canonical";
   std::string arc_mode = "disabled";
@@ -84,21 +86,6 @@ struct Objc3IRFrontendMetadata : Objc3IRFrontendRuntimeSupportMetadata,
   std::size_t property_getter_selector_entries = 0;
   std::size_t property_setter_selector_entries = 0;
   bool deterministic_property_attribute_handoff = false;
-  std::string lowering_dispatch_dispatch_control_replay_key;
-  std::size_t dispatch_dispatch_control_lowering_direct_call_candidate_sites = 0;
-  std::size_t dispatch_dispatch_control_lowering_direct_members_defaulted_sites =
-      0;
-  std::size_t dispatch_dispatch_control_lowering_dynamic_opt_out_sites = 0;
-  std::size_t dispatch_dispatch_control_lowering_final_container_sites = 0;
-  std::size_t dispatch_dispatch_control_lowering_sealed_container_sites = 0;
-  std::size_t dispatch_dispatch_control_lowering_override_legality_sites = 0;
-  std::size_t dispatch_dispatch_control_lowering_metadata_preserved_callable_sites =
-      0;
-  std::size_t
-      dispatch_dispatch_control_lowering_metadata_preserved_container_sites = 0;
-  std::size_t dispatch_dispatch_control_lowering_guard_blocked_sites = 0;
-  std::size_t dispatch_dispatch_control_lowering_contract_violation_sites = 0;
-  bool deterministic_dispatch_dispatch_control_lowering_handoff = false;
   std::string lowering_interop_interop_replay_key;
   std::size_t interop_interop_lowering_foreign_callable_sites = 0;
   std::size_t interop_interop_lowering_c_foreign_callable_sites = 0;
@@ -205,19 +192,6 @@ struct Objc3IRFrontendMetadata : Objc3IRFrontendRuntimeSupportMetadata,
   bool metaprogramming_module_replay_runtime_import_artifact_ready = false;
   bool metaprogramming_module_replay_separate_compilation_preservation_ready = false;
   bool deterministic_metaprogramming_module_interface_replay_handoff = false;
-  std::string lowering_dispatch_dispatch_metadata_interface_preservation_key;
-  std::size_t dispatch_dispatch_metadata_local_direct_callable_record_count = 0;
-  std::size_t dispatch_dispatch_metadata_local_final_callable_record_count = 0;
-  std::size_t dispatch_dispatch_metadata_local_final_container_record_count = 0;
-  std::size_t dispatch_dispatch_metadata_local_sealed_container_record_count = 0;
-  std::size_t dispatch_dispatch_metadata_imported_module_count = 0;
-  std::size_t dispatch_dispatch_metadata_imported_direct_callable_record_count = 0;
-  std::size_t dispatch_dispatch_metadata_imported_final_callable_record_count = 0;
-  std::size_t dispatch_dispatch_metadata_imported_final_container_record_count = 0;
-  std::size_t dispatch_dispatch_metadata_imported_sealed_container_record_count = 0;
-  bool dispatch_dispatch_metadata_runtime_import_artifact_ready = false;
-  bool dispatch_dispatch_metadata_separate_compilation_preservation_ready = false;
-  bool deterministic_dispatch_dispatch_metadata_interface_handoff = false;
   std::string lowering_ownership_system_extension_replay_key;
   std::size_t ownership_system_extension_lowering_cleanup_hook_sites = 0;
   std::size_t ownership_system_extension_lowering_resource_local_sites = 0;
