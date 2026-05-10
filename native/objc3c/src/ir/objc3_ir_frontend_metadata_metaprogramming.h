15b412f4785b312fcb70b535b539bd8b5da0970d
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 04:11:16 -0400

    HC split IR metaprogramming module replay metadata

--- native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming.h
+++ native/objc3c/src/ir/objc3_ir_frontend_metadata_metaprogramming.h
@@ -1,32 +1,8 @@
 #pragma once
 
-#include <cstddef>
-#include <string>
-
 #include "ir/objc3_ir_frontend_metadata_metaprogramming_expansion.h"
+#include "ir/objc3_ir_frontend_metadata_metaprogramming_module_replay.h"
 
 struct Objc3IRFrontendMetaprogrammingMetadata
-    : Objc3IRFrontendMetaprogrammingExpansionMetadata {
-  std::string lowering_metaprogramming_module_interface_replay_preservation_key;
-  std::size_t metaprogramming_module_replay_local_derive_method_count = 0;
-  std::size_t metaprogramming_module_replay_local_macro_artifact_count = 0;
-  std::size_t
-      metaprogramming_module_replay_local_interface_property_behavior_artifact_count = 0;
-  std::size_t
-      metaprogramming_module_replay_local_implementation_property_behavior_artifact_count =
-          0;
-  std::size_t metaprogramming_module_replay_local_runtime_method_list_count = 0;
-  std::size_t metaprogramming_module_replay_imported_module_count = 0;
-  std::size_t metaprogramming_module_replay_imported_derive_method_count = 0;
-  std::size_t metaprogramming_module_replay_imported_macro_artifact_count = 0;
-  std::size_t
-      metaprogramming_module_replay_imported_interface_property_behavior_artifact_count =
-          0;
-  std::size_t
-      metaprogramming_module_replay_imported_implementation_property_behavior_artifact_count =
-          0;
-  std::size_t metaprogramming_module_replay_imported_runtime_method_list_count = 0;
-  bool metaprogramming_module_replay_runtime_import_artifact_ready = false;
-  bool metaprogramming_module_replay_separate_compilation_preservation_ready = false;
-  bool deterministic_metaprogramming_module_interface_replay_handoff = false;
-};
+    : Objc3IRFrontendMetaprogrammingExpansionMetadata,
+      Objc3IRFrontendMetaprogrammingModuleReplayMetadata {};
