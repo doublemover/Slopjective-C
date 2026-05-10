9941ca2f6378a16288b8131550c7ca645df40213
doublemover <153689082+doublemover@users.noreply.github.com>
2026-05-10 01:58:07 -0400

    refactor(parser): split ObjC parameter type declarations

--- native/objc3c/src/parse/objc3_parser_core.cpp
+++ native/objc3c/src/parse/objc3_parser_core.cpp
@@ -5,7 +5,7 @@
 #include "parse/objc3_parser_core_surface_finalizers.inc"
 #include "parse/objc3_parser_core_objc_declarations_containers.inc"
 #include "parse/objc3_parser_core_objc_declarations_functions.inc"
-#include "parse/objc3_parser_core_objc_declarations.inc"
+#include "parse/objc3_parser_core_objc_declarations_parameter_types.inc"
 #include "parse/objc3_parser_core_method_parameters.inc"
 #include "parse/objc3_parser_core_statements.inc"
 #include "parse/objc3_parser_core_blocks_and_expressions.inc"
